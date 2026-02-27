1c3d72a4aa2cf056e1df8d6a480d5c2a894a0306
João Copetti <joaocopetti@proton.me>
2026-02-27 13:26:56 -0300

    refactor: remove ziggy alias, use vendor folder instead

--- resources/js/ziggy.js
+++
@@ -1,5 +0,0 @@
-const Ziggy = {"url":"http:\/\/localhost","port":null,"defaults":{},"routes":{"debugbar.openhandler":{"uri":"_debugbar\/open","methods":["GET","HEAD"]},"debugbar.cache.delete":{"uri":"_debugbar\/cache\/{key}","methods":["DELETE"],"wheres":{"key":".*"},"parameters":["key"]},"debugbar.queries.explain":{"uri":"_debugbar\/queries\/explain","methods":["POST"]},"debugbar.clockwork":{"uri":"_debugbar\/clockwork\/{id}","methods":["GET","HEAD"],"parameters":["id"]},"debugbar.assets":{"uri":"_debugbar\/assets","methods":["GET","HEAD"]},"main":{"uri":"\/","methods":["GET","HEAD"]},"storage.local":{"uri":"storage\/{path}","methods":["GET","HEAD"],"wheres":{"path":".*"},"parameters":["path"]},"storage.local.upload":{"uri":"storage\/{path}","methods":["PUT"],"wheres":{"path":".*"},"parameters":["path"]}}};
-if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
-  Object.assign(Ziggy.routes, window.Ziggy.routes);
-}
-export { Ziggy };
