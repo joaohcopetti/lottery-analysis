826b8faf09a847df15f14b82223c677a89d344de
João Copetti <joaocopetti@proton.me>
2026-02-27 13:28:16 -0300

    feat(button): improve button component

--- resources/js/components/button/AppButton.vue
+++ resources/js/components/button/AppButton.vue
@@ -1,37 +1,71 @@
 <script setup lang="ts">
+import IPhCircleNotch from 'virtual:icons/ph/circle-notch-bold'
 import { computed, useSlots } from 'vue'
-import { AppButtonProps, BUTTON_COLOR_CLASS_MAP } from './types'
+import { BUTTON_COLOR_CLASS_MAP } from './types'
+
+export type AppButtonProps = {
+  label?: string
+  icon?: any
+  color?: keyof typeof BUTTON_COLOR_CLASS_MAP
+  loading?: boolean
+  disabled?: boolean
+}
 
 const props = withDefaults(defineProps<AppButtonProps>(), {
   label: '',
   icon: '',
   color: 'primary',
   iconOnly: false,
+  loading: false,
+  disabled: false,
 })
 
 const isIconOnly = computed(() => props.icon && !props.label && !useSlots()['label'])
+const isDisabled = computed(() => props.disabled || props.loading)
+
+const buttonClasses = computed(() => {
+  const classes = []
+
+  if (isIconOnly.value) {
+    classes.push('flex w-10 items-center justify-center')
+  }
+
+  if (isDisabled.value) {
+    classes.push('cursor-default bg-gray-300 text-gray-500')
+  }
+
+  if (!isDisabled.value) {
+    classes.push(BUTTON_COLOR_CLASS_MAP[props.color].default)
+    classes.push('cursor-pointer')
+  }
+
+  return classes
+})
 </script>
 
 <template>
   <button
-    class="h-10 cursor-pointer rounded-md px-2 transition-all"
-    :class="[
-      BUTTON_COLOR_CLASS_MAP[color],
-      {
-        'flex w-10 items-center justify-center': isIconOnly,
-      },
-    ]"
+    :disabled="isDisabled"
+    class="h-10 rounded-md px-3 transition-all"
+    :class="buttonClasses"
   >
-    <Component
-      :is="icon"
-      v-if="icon"
-    />
+    <div class="flex items-center gap-1">
+      <Component
+        :is="loading ? IPhCircleNotch : icon"
+        v-if="icon || loading"
+        :class="{
+          'animate-spin': loading,
+        }"
+      />
 
-    <slot
-      v-if="$slots['label']"
-      name="label"
-    />
+      <div>
+        <slot
+          v-if="$slots['label']"
+          name="label"
+        />
 
-    <template v-else>{{ label }}</template>
+        <template v-else>{{ label }}</template>
+      </div>
+    </div>
   </button>
 </template>
--- resources/js/components/button/types.ts
+++ resources/js/components/button/types.ts
@@ -1,11 +1,14 @@
 export const BUTTON_COLOR_CLASS_MAP = {
-  primary: 'bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white',
-  light: 'bg-white hover:bg-gray-200 active:bg-gray-300 text-gray-900',
-  danger: 'bg-red-700 hover:bg-red-800 active:bg-red-900 text-white',
-}
-
-export type AppButtonProps = {
-  label?: string
-  icon?: any
-  color?: keyof typeof BUTTON_COLOR_CLASS_MAP
+  primary: {
+    default: 'bg-blue-600 hover:bg-blue-700 active:bg-blue-800 text-white',
+    disabled: 'bg-blue-200',
+  },
+  light: {
+    default: 'bg-white hover:bg-gray-200 active:bg-gray-300 text-gray-900',
+    disabled: 'bg-gray-200',
+  },
+  danger: {
+    default: 'bg-red-700 hover:bg-red-800 active:bg-red-900 text-white',
+    disabled: 'bg-red-200',
+  },
 }
