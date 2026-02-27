374582611da6b48ec086503382c0e27a8da77e01
João Copetti <joaocopetti@proton.me>
2026-02-27 10:52:25 -0300

    feat: add badge component

---
+++ resources/js/components/badge/AppBadge.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const BADGE_COLORS_CLASS_MAP = {
+  primary: 'bg-blue-500 text-blue-100',
+  warning: 'bg-amber-500 text-amber-900',
+}
+
+const SIZES_CLASS_MAP = {
+  sm: 'px-1.5 py-0.5 text-[10px]',
+  md: 'px-2 py-1 text-[12px]',
+}
+
+withDefaults(
+  defineProps<{
+    color?: keyof typeof BADGE_COLORS_CLASS_MAP
+    size?: keyof typeof SIZES_CLASS_MAP
+  }>(),
+  {
+    color: 'primary',
+    size: 'md',
+  },
+)
+</script>
+
+<template>
+  <span
+    class="flex items-center rounded-full font-bold"
+    :class="[BADGE_COLORS_CLASS_MAP[color], SIZES_CLASS_MAP[size]]"
+  >
+    <slot />
+  </span>
+</template>
