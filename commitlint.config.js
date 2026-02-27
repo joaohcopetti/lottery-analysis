9f49dda1955837988e6bebbdcc9640417473dfc5
João Copetti <joaocopetti@proton.me>
2026-02-27 14:54:12 -0300

    chore(lint-config): add new enum scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,10 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [2, 'always', ['llm', 'migration']],
+    'scope-enum': [
+      2,
+      'always',
+      ['llm', 'migration', 'main-controller', 'date-picker', 'dropdown', 'lint-config'],
+    ],
   },
 }
